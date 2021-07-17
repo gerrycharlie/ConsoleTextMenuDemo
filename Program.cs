230daa1cbdea64f67cac2a665200e34a6470939d
Gerard Mijares <gerardmijares@Gerards-Mini.lan>
2021-07-17 14:35:44 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTextMenuDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
